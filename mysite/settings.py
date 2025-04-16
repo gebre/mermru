8b63cf661b97c5c57b53196f23ea3b88da6fc844
gebre <destinycome@gmail.com>
2025-04-17 01:43:32 +0200

    added app 3

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,14 @@ SECRET_KEY = 'django-insecure-ojf(f!z5_2^$kb_#@(!zff=s=o2@uucq82)uxjv*)929hn)ij$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.124.55.189','5.156.32.215']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
