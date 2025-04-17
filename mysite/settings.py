a1c7c4c2ae69fcaa6d764d96fbcedc4c039f599a
gebre <destinycome@gmail.com>
2025-04-18 01:24:11 +0200

    database

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,12 +75,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mermru',
+        'USER': 'admin',
+        'PASSWORD': 'gere?1PM',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
